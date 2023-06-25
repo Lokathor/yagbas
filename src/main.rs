1bfe859197b6f5e80141fcc75e681fc3691dd701
Lokathor <zefria@gmail.com>
2023-06-24 23:47:18 -0600

    add a test program, using the "yag" extension.

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,7 @@ use chumsky::{input::Input, prelude::*, span::SimpleSpan, IterParser, Parser};
 use yagbas::{lexer::Token, parser::*};
 
 fn main() {
-  let prog = r#"
-    const FOO = 1_2;
-    const BAR = -2;
-    const ZAP = %0101_012;
-    const HEK = $FF00;
-    section main [rom0] {
-      // comment1
-      label1:
-        ld a FOO;
-        ld b $4_6;
-        ld c %101_010;
-        d = $FF;
-      1: /* foo bar baz */
-        inc c;
-        zero_bytes!(3);
-        jr 1b;
-    }
-  "#;
+  let prog = include_str!("../tests/hello.yag");
 
   let tokens: Vec<(Token, SimpleSpan)> = match strip_comments(Token::lexer(prog).spanned()) {
     Ok(tokens) => tokens,
@@ -31,13 +14,12 @@ fn main() {
       return;
     }
   };
-  println!("Tokens: {tokens:?}");
 
   let x = item_parser()
     .repeated()
     .collect::<Vec<_>>()
+    .then_ignore(end())
     .parse(tokens.spanned(SimpleSpan::from(0..prog.len())));
-  println!("{x:?}");
   for item in x.output().unwrap() {
     match item {
       Item::Const(c) => {
@@ -45,7 +27,11 @@ fn main() {
         let val = interpret_num(c.0.val.0);
         println!("Const> Name: {name:?}, Val: {val:?}");
       }
-      Item::Section(_) => continue,
+      Item::Section(s) => {
+        let name = s.0.name.0;
+        let locations = &s.0.locations;
+        println!("Section> Name: {name}");
+      }
     }
   }
 }
--- tests/numbers.rs
+++ tests/numbers.rs
@@ -3,23 +3,28 @@ use yagbas::{
   parser::{interpret_num, strip_comments, InterpretNumErr},
 };
 
-fn num_test(s: &str, t: Token, r: Result<i32, InterpretNumErr>) {
+fn num_test(s: &str, r: Result<i32, InterpretNumErr>) {
   let tokens: Vec<Token> =
     strip_comments(Token::lexer(s).spanned()).unwrap().into_iter().map(|t| t.0).collect();
   assert_eq!(1, tokens.len(), "Too Many Tokens: {tokens:?}");
-  assert_eq!(t, tokens[0]);
   assert_eq!(r, interpret_num(tokens[0].unwrap_num()));
 }
 
 #[test]
-fn numbers() {
+fn number_parsing() {
   let x = [
-    ("0", Token::Num("0"), Ok(0)),
-    ("1", Token::Num("1"), Ok(1)),
-    ("-1", Token::Num("-1"), Ok(-1)),
-    (" $FF_00", Token::Num("$FF_00"), Ok(0xFF00)),
+    ("0", Ok(0)),
+    ("0011", Ok(11)),
+    ("1", Ok(1)),
+    ("1234", Ok(1234)),
+    ("-1", Ok(-1)),
+    ("10", Ok(10)),
+    ("$FF_00", Ok(0xFF00)),
+    ("%1010", Ok(0b1010)),
+    ("0xABCD", Ok(0xABCD)),
+    ("0b1100", Ok(0b1100)),
   ];
-  for (s, t, r) in x {
-    num_test(s, t, r)
+  for (s, r) in x {
+    num_test(s, r)
   }
 }
