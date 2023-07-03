2669cc75ea5963a7b84630d9b608ef62fd7012dd
Lokathor <zefria@gmail.com>
2023-07-02 22:08:28 -0600

    naming

--- src/parser/item.rs
+++ src/parser/item.rs
@@ -3,7 +3,7 @@ use super::*;
 #[derive(Debug, Clone)]
 pub enum Item {
   ConstDecl(ConstDecl),
-  Section(Section),
+  SectionDecl(SectionDecl),
 }
 impl Item {
   pub fn parser<'a, I>() -> impl Parser<'a, I, Self, ErrRichTokenTree<'a>>
@@ -11,7 +11,7 @@ impl Item {
     I: ValueInput<'a, Token = TokenTree, Span = SimpleSpan>,
   {
     let const_decl = ConstDecl::parser().map(Item::ConstDecl);
-    let section = Section::parser().map(Item::Section);
+    let section = SectionDecl::parser().map(Item::SectionDecl);
 
     const_decl.or(section)
   }
