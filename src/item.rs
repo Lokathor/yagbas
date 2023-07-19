5cb01278cce201b1d5db8abcb8fcf6e3ee07db76
Lokathor <zefria@gmail.com>
2023-07-19 09:24:29 -0600

    tried to recover but this doesn't seem to pass the test like i thought it would.

--- src/item.rs
+++ src/item.rs
@@ -11,8 +11,10 @@ impl Item {
   pub fn parser<'a>(
   ) -> impl Parser<'a, TokenTreeSlice<'a>, Self, ErrRichTokenTree<'a>> + Clone {
     let const_decl = ConstDecl::parser().map(Self::ConstDecl);
+    let item_error =
+      none_of([Lone(Punct(';'))]).repeated().then(semicolon()).to(Self::ItemError);
 
-    choice((const_decl,))
+    choice((const_decl,)).recover_with(via_parser(item_error))
   }
 }
 
@@ -26,10 +28,13 @@ impl ConstDecl {
   pub fn parser<'a>(
   ) -> impl Parser<'a, TokenTreeSlice<'a>, Self, ErrRichTokenTree<'a>> + Clone {
     let kw_const = just(Lone(KwConst));
+    let ident = ident().map_with_span(id2);
+    let expr = ConstExpr::parser().map_with_span(id2);
+    //
     kw_const
-      .ignore_then(ident().map_with_span(id2))
+      .ignore_then(ident)
       .then_ignore(equal())
-      .then(ConstExpr::parser().map_with_span(id2))
+      .then(expr)
       .then_ignore(semicolon())
       .map(|(name, expr)| Self { name, expr })
   }
