2111d3df9e9bf4f0ed0c45c33fa69ef35fb884f2
Lokathor <zefria@gmail.com>
2024-10-26 14:15:33 -0600

    actually that won't work with immediate values

--- src/asm.rs
+++ src/asm.rs
@@ -17,34 +17,3 @@ pub enum Asm {
   Return(Cond, StrID),
   Jump(Cond, StrID),
 }
-impl Asm {
-  #[inline]
-  #[must_use]
-  pub const fn bytes(self) -> &'static [u8] {
-    match self {
-      Asm::Label(_) => &[],
-      Asm::Nop => &[0x00],
-      Asm::Call(cond, _) => match cond {
-        Cond::NonZero => &[0xC4, 0xFC, 0xFD],
-        Cond::NoCarry => &[0xD4, 0xFC, 0xFD],
-        Cond::Zero => &[0xCC, 0xFC, 0xFD],
-        Cond::Carry => &[0xDC, 0xFC, 0xFD],
-        Cond::Always => &[0xCD, 0xFC, 0xFD],
-      },
-      Asm::Return(cond, _) => match cond {
-        Cond::NonZero => &[0xC0, 0xFC, 0xFD],
-        Cond::NoCarry => &[0xD0, 0xFC, 0xFD],
-        Cond::Zero => &[0xC8, 0xFC, 0xFD],
-        Cond::Carry => &[0xD8, 0xFC, 0xFD],
-        Cond::Always => &[0xC9, 0xFC, 0xFD],
-      },
-      Asm::Jump(cond, _) => match cond {
-        Cond::NonZero => &[0xC2, 0xFC, 0xFD],
-        Cond::NoCarry => &[0xD2, 0xFC, 0xFD],
-        Cond::Zero => &[0xCA, 0xFC, 0xFD],
-        Cond::Carry => &[0xDA, 0xFC, 0xFD],
-        Cond::Always => &[0xC3, 0xFC, 0xFD],
-      },
-    }
-  }
-}
