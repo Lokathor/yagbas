311b9c5c05e9ee0663fec403c9d935d309939213
Lokathor <zefria@gmail.com>
2024-10-26 08:05:26 -0600

    using FC/FD as placeholders, make a fn for the bytes of each asm output.

--- src/asm.rs
+++ src/asm.rs
@@ -1,12 +1,11 @@
-
 use crate::str_id::StrID;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Cond {
-  Zero,
   NonZero,
-  Carry,
   NoCarry,
+  Zero,
+  Carry,
   Always,
 }
 
@@ -17,4 +16,35 @@ pub enum Asm {
   Call(Cond, StrID),
   Return(Cond, StrID),
   Jump(Cond, StrID),
+}
+impl Asm {
+  #[inline]
+  #[must_use]
+  pub const fn bytes(self) -> &'static [u8] {
+    match self {
+      Asm::Label(str_id) => &[],
+      Asm::Nop => &[0x00],
+      Asm::Call(cond, _) => match cond {
+        Cond::NonZero => &[0xC4, 0xFC, 0xFD],
+        Cond::NoCarry => &[0xD4, 0xFC, 0xFD],
+        Cond::Zero => &[0xCC, 0xFC, 0xFD],
+        Cond::Carry => &[0xDC, 0xFC, 0xFD],
+        Cond::Always => &[0xCD, 0xFC, 0xFD],
+      },
+      Asm::Return(cond, str_id) => match cond {
+        Cond::NonZero => &[0xC0, 0xFC, 0xFD],
+        Cond::NoCarry => &[0xD0, 0xFC, 0xFD],
+        Cond::Zero => &[0xC8, 0xFC, 0xFD],
+        Cond::Carry => &[0xD8, 0xFC, 0xFD],
+        Cond::Always => &[0xC9, 0xFC, 0xFD],
+      },
+      Asm::Jump(cond, str_id) => match cond {
+        Cond::NonZero => &[0xC2, 0xFC, 0xFD],
+        Cond::NoCarry => &[0xD2, 0xFC, 0xFD],
+        Cond::Zero => &[0xCA, 0xFC, 0xFD],
+        Cond::Carry => &[0xDA, 0xFC, 0xFD],
+        Cond::Always => &[0xC3, 0xFC, 0xFD],
+      },
+    }
+  }
 }
