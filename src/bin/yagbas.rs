02c3cdab3fbec60e678d2306b3c89ce867bc7efd
Lokathor <zefria@gmail.com>
2024-10-27 11:40:13 -0600

    how do we turn parsing on even

--- src/bin/yagbas.rs
+++ src/bin/yagbas.rs
@@ -1,13 +1,13 @@
 #![allow(unused)]
 #![allow(clippy::type_complexity)]
 
+use clap::{Args, Parser, Subcommand};
 use std::{
   collections::HashMap,
   path::{Path, PathBuf},
 };
-
-use clap::{Args, Parser, Subcommand};
 use yagbas::{
+  parsing::token_tree_p,
   src_files::{FileSpan, FileSpanned, SrcFile, SrcID},
   str_id::StrID,
   token::Token,
@@ -31,6 +31,8 @@ pub struct Cli {
 pub enum Commands {
   /// Prints all tokens within the source files given.
   Tokenize(TokenizeArgs),
+  /// Prints all token trees within the source files given.
+  Trees(TreesArgs),
 }
 
 #[derive(Args, Debug, Clone)]
@@ -39,10 +41,17 @@ pub struct TokenizeArgs {
   pub files: Vec<String>,
 }
 
+#[derive(Args, Debug, Clone)]
+pub struct TreesArgs {
+  /// One or more source files to make into token trees.
+  pub files: Vec<String>,
+}
+
 pub fn main() {
   let cli = Cli::parse();
   match cli.command {
     Commands::Tokenize(args) => do_tokenize(args),
+    Commands::Trees(args) => do_trees(args),
   }
 }
 
@@ -57,5 +66,27 @@ pub fn do_tokenize(args: TokenizeArgs) {
     };
     let tokens: Vec<FileSpanned<Token>> = src_file.iter_tokens().collect();
     println!("{filename}: {tokens:?}");
+  }
+}
+
+pub fn do_trees(args: TreesArgs) {
+  for filename in &args.files {
+    let src_file = match SrcFile::read_from_path(&filename) {
+      Ok(src_file) => src_file,
+      Err(io_error) => {
+        eprintln!("{filename}: File IO Error: {io_error}");
+        continue;
+      }
+    };
+    let tokens: Vec<FileSpanned<Token>> = src_file.iter_tokens().collect();
+    if tokens.is_empty() {
+      continue;
+    }
+    let last_span = tokens.last().map(|token| token._span).unwrap();
+    let end_span = FileSpan { start: last_span.end, ..last_span };
+    use chumsky::{input::BorrowInput, prelude::Parser};
+    let parse_result = token_tree_p()
+      .parse(tokens.as_slice().map(end_span, |fs| (fs._payload, fs._span)));
+    println!("{filename}: {parse_result:?}");
   }
 }
