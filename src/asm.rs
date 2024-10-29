ac4452bb3d35477815ca4096e280f446e455c647
Lokathor <zefria@gmail.com>
2024-10-28 23:48:58 -0600

    be able to print out asm, but we still don't generate any asm.

--- src/asm.rs
+++ src/asm.rs
@@ -1,6 +1,30 @@
 use crate::str_id::StrID;
 
 #[derive(Debug, Clone, Copy)]
+pub enum Asm {
+  Label(StrID),
+  Nop,
+  Call(Cond, StrID),
+  Return(Cond),
+  Jump(Cond, StrID),
+}
+impl core::fmt::Display for Asm {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use core::fmt::Write;
+    match self {
+      Asm::Label(name) => write!(f, "{name}:"),
+      Asm::Nop => write!(f, "nop"),
+      Asm::Call(Cond::Always, target) => write!(f, "call {target}"),
+      Asm::Call(cond, target) => write!(f, "call {cond}, {target}"),
+      Asm::Return(Cond::Always) => write!(f, "ret"),
+      Asm::Return(cond) => write!(f, "ret {cond}"),
+      Asm::Jump(Cond::Always, target) => write!(f, "jp {target}"),
+      Asm::Jump(cond, target) => write!(f, "jp {cond}, {target}"),
+    }
+  }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum Cond {
   NonZero,
   NoCarry,
@@ -8,12 +32,17 @@ pub enum Cond {
   Carry,
   Always,
 }
-
-#[derive(Debug, Clone, Copy)]
-pub enum Asm {
-  Label(StrID),
-  Nop,
-  Call(Cond, StrID),
-  Return(Cond, StrID),
-  Jump(Cond, StrID),
+impl core::fmt::Display for Cond {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use core::fmt::Write;
+    match self {
+      Cond::NonZero => write!(f, "nz"),
+      Cond::NoCarry => write!(f, "nc"),
+      Cond::Zero => write!(f, "z"),
+      Cond::Carry => write!(f, "c"),
+      // Note(Lokathor): This variant shouldn't actually be printed, but if it
+      // does get printed somehow we'll just fudge it.
+      Cond::Always => write!(f, "al"),
+    }
+  }
 }
--- src/statement.rs
+++ src/statement.rs
@@ -1,6 +1,10 @@
 use crate::{src_files::FileSpanned, str_id::StrID, token_tree::TokenTree};
 use core::sync::atomic::{AtomicUsize, Ordering};
 
+/// A "single chunk of code" in yagbas.
+///
+/// Most statements are "one line" of code, but the variants for control flow
+/// have a body of inner statements that they cover.
 #[derive(Debug, Clone)]
 pub enum Statement {
   Return,
@@ -36,5 +40,18 @@ impl Loop {
     name: StrID, statements: Vec<FileSpanned<Statement>>,
   ) -> Self {
     Self { name, statements, id: Self::get_next_id() }
+  }
+
+  #[inline]
+  pub fn make_start_name(&self) -> String {
+    let x = self.id;
+    let name = self.name;
+    format!(".loop{x}#{name}#start")
+  }
+  #[inline]
+  pub fn make_end_name(&self) -> String {
+    let x = self.id;
+    let name = self.name;
+    format!(".loop{x}#{name}#end")
   }
 }
