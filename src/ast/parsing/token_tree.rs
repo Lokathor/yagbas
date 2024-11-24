98f730b84f7501210f1d3c4299d9f7b830f8f474
Lokathor <zefria@gmail.com>
2024-11-24 14:15:15 -0700

    cleanup to make the main body more readable.

--- src/ast/parsing/token_tree.rs
+++ src/ast/parsing/token_tree.rs
@@ -10,67 +10,31 @@ where
     let base = tt.map_with(|tts, e| FileSpanned::new(tts, e.span())).repeated();
 
     // Looks like `{ ... }`
-    let braces = {
-      let open_brace = select! {
-        OpBrace => (),
-      }
-      .labelled("open_brace")
+    let braces = base
+      .clone()
+      .collect()
+      .delimited_by(open_brace_p(), close_brace_p())
+      .map(TokenTree::Braces)
+      .labelled("braces_group")
       .as_context();
-      let close_brace = select! {
-        ClBrace => (),
-      }
-      .labelled("close_brace")
-      .as_context();
-      base
-        .clone()
-        .collect()
-        .delimited_by(open_brace, close_brace)
-        .map(TokenTree::Braces)
-        .labelled("braces_group")
-        .as_context()
-    };
 
     // Looks like `[ ... ]`
-    let brackets = {
-      let open_bracket = select! {
-        OpBracket => (),
-      }
-      .labelled("open_bracket")
+    let brackets = base
+      .clone()
+      .collect()
+      .delimited_by(open_bracket_p(), close_bracket_p())
+      .map(TokenTree::Brackets)
+      .labelled("brackets_group")
       .as_context();
-      let close_bracket = select! {
-        ClBracket => (),
-      }
-      .labelled("close_bracket")
-      .as_context();
-      base
-        .clone()
-        .collect()
-        .delimited_by(open_bracket, close_bracket)
-        .map(TokenTree::Brackets)
-        .labelled("brackets_group")
-        .as_context()
-    };
 
     // Looks like `( ... )`
-    let parens = {
-      let open_paren = select! {
-        OpParen => (),
-      }
-      .labelled("open_paren")
+    let parens = base
+      .clone()
+      .collect()
+      .delimited_by(open_paren_p(), close_paren_p())
+      .map(TokenTree::Parens)
+      .labelled("parens_group")
       .as_context();
-      let close_paren = select! {
-        ClParen => (),
-      }
-      .labelled("close_paren")
-      .as_context();
-      base
-        .clone()
-        .collect()
-        .delimited_by(open_paren, close_paren)
-        .map(TokenTree::Parens)
-        .labelled("parens_group")
-        .as_context()
-    };
 
     // Looks like something that does *NOT* open or close one of the other
     // types.
@@ -85,15 +49,19 @@ where
         CommentSingle => (),
       };
       // Looks like `/* ... */`
-      let block_start = select! {
+      let comment_block_start = select! {
         CommentBlockStart => (),
-      };
-      let block_end = select! {
+      }
+      .labelled("comment_block_start")
+      .as_context();
+      let comment_block_end = select! {
         CommentBlockEnd => (),
-      };
+      }
+      .labelled("comment_block_end")
+      .as_context();
       let block_comment = base
         .clone()
-        .delimited_by(block_start, block_end)
+        .delimited_by(comment_block_start, comment_block_end)
         .ignored()
         .labelled("block_comment")
         .as_context();
@@ -108,4 +76,79 @@ where
 
     x
   })
+}
+
+/// Parses an `OpBrace`, which is then discarded.
+pub fn open_brace_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    OpBrace => (),
+  }
+  .labelled("open_brace")
+  .as_context()
+}
+/// Parses a `ClBrace`, which is then discarded.
+pub fn close_brace_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    ClBrace => (),
+  }
+  .labelled("close_brace")
+  .as_context()
+}
+
+/// Parses an `OpBracket`, which is then discarded.
+pub fn open_bracket_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    OpBracket => (),
+  }
+  .labelled("open_bracket")
+  .as_context()
+}
+/// Parses a `ClBracket`, which is then discarded.
+pub fn close_bracket_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    ClBracket => (),
+  }
+  .labelled("close_bracket")
+  .as_context()
+}
+
+/// Parses an `OpParen`, which is then discarded.
+pub fn open_paren_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    OpParen => (),
+  }
+  .labelled("open_paren")
+  .as_context()
+}
+/// Parses a `ClParen`, which is then discarded.
+pub fn close_paren_p<'src, I>(
+) -> impl Parser<'src, I, (), ErrRichToken<'src>> + Clone
+where
+  I: BorrowInput<'src, Token = Token, Span = FileSpan> + ValueInput<'src>,
+{
+  select! {
+    ClParen => (),
+  }
+  .labelled("close_paren")
+  .as_context()
 }
