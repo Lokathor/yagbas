7af4ef063f7481b95b1662b4174bbc915999ad0c
Lokathor <zefria@gmail.com>
2024-11-30 00:06:44 -0700

    add more Expression variants

--- src/ast/data/expression.rs
+++ src/ast/data/expression.rs
@@ -5,9 +5,57 @@ pub enum Expression {
   NumLit(FileSpanned<StrID>),
   Ident(FileSpanned<StrID>),
   Register(FileSpanned<Register>),
-  /// `x = y`
-  Assign(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  Bool(FileSpanned<bool>),
+
+  /// `x.y`
+  Dot(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `-x`
+  Neg(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `&x`
+  Ref(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x * y`
+  Mul(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x / y`
+  Div(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x % y`
+  Mod(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x + y`
+  Add(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x - y`
+  Sub(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x << y`
+  ShiftLeft(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x >> y`
+  ShiftRight(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x & y`
+  BitAnd(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x ^ y`
+  BitXor(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x | y`
+  BitOr(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
   /// `x == y`
   Eq(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x != y`
+  Ne(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x < y`
+  Lt(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x > y`
+  Gt(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x <= y`
+  Le(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+  /// `x >= y`
+  Ge(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
+  /// `x = y`
+  Assign(Box<FileSpanned<Self>>, Box<FileSpanned<Self>>),
+
   ExpressionError,
 }
