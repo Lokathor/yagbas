65e40a6e431c6cf4246e9539f5a1f954b9600106
Daniel Henry-Mantilla <134883366+danielhenrymantilla-codespace@users.noreply.github.com>
2025-01-13 17:33:29 +0000

    WIP: finish renaming `InternalIterator{Ref,Mut}` as `InternalIteratorRec`

--- src/internal_iterator_rec.rs
+++ src/internal_iterator_rec.rs
@@ -1,22 +1,57 @@
 use super::*;
 
-pub trait InternalIteratorRef: InternalIterator<Item = Self::ItemRef> {
-  type ItemRef;
-  fn try_for_each_ref<R, F>(self, f: &mut F) -> ControlFlow<R>
+/// A `Rec`ursive-`impl`-friendly extension trait of [`InternalIterator`].
+///
+/// Whenever `impl`ementing [`InternalIterator`] is to be done by recurse-delegating
+/// to *multiple* sub-fields, then the `mut f: F = impl FnMut(…)` visitor cannot be used
+/// by value, and a `&mut f` borrow of it needs to be used.
+///
+/// But, given the recursion, if the child field is of a type which may ultimately
+/// contain the parent (as is often the case with mutually recursive AST definitions),
+/// it means we will end up back at our original function call, but with `F` having
+/// become `&mut F`.
+///
+/// And even if this recursion will terminate at runtime, the type system
+/// *and codegen* / `<F>`-generic-code monorphization stages don't know of this
+/// (similar problem to that of a recursive type).
+///
+/// This means that codegen will try to monomorphize the code for `<F>`, then for `<&mut F>`,
+/// then for `<&mut &mut F>`, _etc._, _ad nauseam_, since there is no termination within the
+/// type system.
+///
+/// This issue, called a "polymorphization error", causes compilation (codegen) to fail.
+///
+/// The solution, then, is to start off a reborrowable `&mut F` arg to begin with.
+/// That way, the recursive stage with *multiple* accesses can keep using this arg
+/// (reborrows of `&mut F`).
+///
+/// Granted, we are technically back at a similar situation (codegen for `&'r mut F`
+/// ultimately requires codegen for `&'slightly_shorter mut F`, and so on), but this
+/// is fine since lifetimes do not affect codegen).
+///
+/// But [`InternalIterator`] offers no such method.
+///
+/// Hence this helper "extension" trait, simply offering such a method, alongside a helper
+/// macro to play the role of the blanket impl (which cannot be written due to orphan rules)
+/// from which the proper [`InternalIterator`] `impl` can be derived (by delegating to our
+/// well-defined `&mut F` case).
+pub trait InternalIteratorRec: InternalIterator<Item = Self::ItemRec> {
+  type ItemRec;
+  fn try_for_each_rec<R, F>(self, f: &mut F) -> ControlFlow<R>
   where
     F: FnMut(Self::Item) -> ControlFlow<R>;
 }
 
 #[macro_export]
-macro_rules! internal_iterator_ref_guts {
+macro_rules! internal_iterator_rec_guts {
   () => {
-    type Item = <Self as InternalIteratorRef>::ItemRef;
+    type Item = <Self as InternalIteratorRec>::ItemRec;
 
     fn try_for_each<R, F>(self, f: F) -> ControlFlow<R>
     where
       F: FnMut(Self::Item) -> ControlFlow<R>,
     {
-      self.try_for_each_ref(&mut { f })
+      self.try_for_each_rec(&mut { f })
     }
   };
 }
