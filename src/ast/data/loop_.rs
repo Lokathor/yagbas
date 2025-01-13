36bcd807ae6c942873a8b8c8372ce500add34876
Lokathor <zefria@gmail.com>
2025-01-13 12:58:42 -0700

    Internal iterator ref (#41)
    * make the trait
    
    * try implementing the trait in our AST.
    
    * `InternalIteratorRef` fix (#42)
    
    * I was being `.lock`ed out of testing the code 🙃
    
    * Fix typo in trait/assoc type usage for `.calls_ref()`
    
    * WIP: `git mv src/internal_iterator_re{f,c}.rs`
    
    * WIP: finish renaming `InternalIterator{Ref,Mut}` as `InternalIteratorRec`
    
    * Rename the call-sites (including `Mut`) accordingly
    
    * WIP: Rename `ItemRec` as `Item` to prevent future such mistakes / bad diagnostics
    
    * Adjust the call-sites accordingly
    
    * WIP: Expose new internal `adhoc_internal_iterator_rec!` helper macro
    
    * Profit™
    
    * rustfmt
    
    🥲
    
    ---------
    
    Co-authored-by: Daniel Henry-Mantilla <134883366+danielhenrymantilla-codespace@users.noreply.github.com>

--- src/ast/data/loop_.rs
+++ src/ast/data/loop_.rs
@@ -40,74 +40,38 @@ impl Loop {
 
   pub fn expressions_mut(
     &mut self,
-  ) -> impl '_ + InternalIteratorMut<ItemMut = &'_ mut FileSpanned<Expression>>
-  {
-    return ExpressionsMut(self);
-    // where:
-    struct ExpressionsMut<'r>(&'r mut Loop);
-    impl<'r> InternalIterator for ExpressionsMut<'r> {
-      internal_iterator_mut_guts! {}
-    }
-
-    impl<'r> InternalIteratorMut for ExpressionsMut<'r> {
-      type ItemMut = &'r mut FileSpanned<Expression>;
-
-      fn try_for_each_mut<R, F>(self, f: &mut F) -> ControlFlow<R>
-      where
-        F: FnMut(Self::Item) -> ControlFlow<R>,
-      {
-        for stmt in self.0.statements.iter_mut() {
-          stmt.expressions_mut().try_for_each_mut(&mut *f)?;
+  ) -> impl '_ + InternalIteratorRec<Item = &'_ mut FileSpanned<Expression>> {
+    adhoc_internal_iterator_rec!(
+      'r, self, |this: &'r mut Loop, yield_| -> &'r mut FileSpanned<Expression> {
+        for stmt in this.statements.iter_mut() {
+          stmt.expressions_mut().try_for_each_rec(yield_)?;
         }
-        ControlFlow::Continue(())
       }
-    }
+    )
   }
 
   pub fn calls_ref(
     &self,
-  ) -> impl '_ + InternalIterator<Item = &'_ FileSpanned<Call>> {
-    return CallsRef(self);
-    // where:
-    struct CallsRef<'r>(&'r Loop);
-    impl<'r> InternalIterator for CallsRef<'r> {
-      type Item = &'r FileSpanned<Call>;
-
-      fn try_for_each<R, F>(self, mut f: F) -> ControlFlow<R>
-      where
-        F: FnMut(Self::Item) -> ControlFlow<R>,
-      {
-        for stmt in self.0.statements.iter() {
-          stmt.calls_ref().try_for_each(&mut f)?;
+  ) -> impl '_ + InternalIteratorRec<Item = &'_ FileSpanned<Call>> {
+    adhoc_internal_iterator_rec!(
+      'r, self, |this: &'r Loop, yield_| -> &'r FileSpanned<Call> {
+        for stmt in this.statements.iter() {
+          stmt.calls_ref().try_for_each_rec(yield_)?;
         }
-        ControlFlow::Continue(())
       }
-    }
+    )
   }
 
   pub fn calls_mut(
     &mut self,
-  ) -> impl '_ + InternalIteratorMut<ItemMut = &'_ mut FileSpanned<Call>> {
-    return CallsMut(self);
-    // where:
-    struct CallsMut<'r>(&'r mut Loop);
-    impl<'r> InternalIterator for CallsMut<'r> {
-      internal_iterator_mut_guts! {}
-    }
-
-    impl<'r> InternalIteratorMut for CallsMut<'r> {
-      type ItemMut = &'r mut FileSpanned<Call>;
-
-      fn try_for_each_mut<R, F>(self, f: &mut F) -> ControlFlow<R>
-      where
-        F: FnMut(Self::Item) -> ControlFlow<R>,
-      {
-        for stmt in self.0.statements.iter_mut() {
-          stmt.calls_mut().try_for_each_mut(&mut *f)?;
+  ) -> impl '_ + InternalIteratorRec<Item = &'_ mut FileSpanned<Call>> {
+    adhoc_internal_iterator_rec!(
+      'r, self, |this: &'r mut Loop, yield_| -> &'r mut FileSpanned<Call> {
+        for stmt in this.statements.iter_mut() {
+          stmt.calls_mut().try_for_each_rec(yield_)?;
         }
-        ControlFlow::Continue(())
       }
-    }
+    )
   }
 
   pub fn write_code(
