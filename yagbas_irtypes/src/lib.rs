e8c270eb81e0c616f2349baf470220d4aafcaee5
Lokathor <zefria@gmail.com>
2025-04-29 12:10:21 -0600

    i think these are all we need to store in the ir

--- yagbas_irtypes/src/lib.rs
+++ yagbas_irtypes/src/lib.rs
@@ -1,1 +1,19 @@
 #![forbid(unsafe_code)]
+
+use str_id::StrID;
+use yagbas_asttypes::S;
+use yagbas_srcfiletypes::{FileData, FileID};
+
+#[derive(Debug, Clone)]
+pub struct IrBitstruct {
+  pub name: S<StrID>,
+  pub file_id: FileID,
+  pub fields: [Option<S<StrID>>; 8],
+}
+
+#[derive(Debug, Clone)]
+pub struct IrStruct {
+  pub name: S<StrID>,
+  pub file_id: FileID,
+  pub fields: Vec<(S<StrID>, S<StrID>)>,
+}
