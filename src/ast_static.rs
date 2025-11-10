a5a6001ef00a0bffd13db487c2b6c7cbc3d96a8a
lokathor <zefria@gmail.com>
2025-11-09 21:00:25 -0700

    clarify the concept we're trying to capture.

--- src/ast_static.rs
+++ src/ast_static.rs
@@ -11,13 +11,22 @@ pub struct AstStatic {
   pub attributes: Vec<AstAttribute>,
   pub file_id: FileID,
   pub total_span: Span32,
-  pub mutability: StaticMutability,
+  pub memory_kind: MemoryKind,
 }
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
-pub enum StaticMutability {
-  #[default]
-  Immutable,
-  Mutable,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum MemoryKind {
+  /// Data in an immutable ROM region.
+  Rom,
+  /// Data in a "normal" mutable RAM region.
+  ///
+  /// Repeated writes to the same RAM location inside of a function are allowed
+  /// to be collapsed to a single write of the final "actual" value.
+  Ram,
+  /// Data that is Memory-mapped IO.
+  ///
+  /// All accesses to MMIO are what Rust/LLVM call "volatile". Every read/write
+  /// needs to actually happen. Repeated writes or reads of the same location
+  /// cannot be combined.
   MemoryMappedIO,
 }
