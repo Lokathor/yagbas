7020074cef575f3672190efa9778300081cbb8a6
lokathor <zefria@gmail.com>
2025-11-18 15:22:28 -0700

    use a fn pointer alias like zesterer suggested (eliminates the unsafe trait impl).

--- src/yag_parser.rs
+++ src/yag_parser.rs
@@ -3,56 +3,32 @@ use chumsky::{
   Parser,
   error::Rich,
   extra::{Full, SimpleState},
-  input::{BorrowInput, Input, ValueInput},
+  input::{BorrowInput, Input, MappedInput, ValueInput},
 };
 
 #[derive(Debug, Clone, Copy)]
-pub struct YagParserState {}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(transparent)]
-pub struct YagParserInput<'src>(&'src [(TokenTree, Span32)]);
-
-impl<'src> Input<'src> for YagParserInput<'src> {
-  type Span = Span32;
-  type Token = TokenTree;
-  type MaybeToken = &'src TokenTree;
-  type Cursor = usize;
-  type Cache = Self;
-
-  fn begin(self) -> (usize, Self) {
-    (0, self)
-  }
-  fn cursor_location(cursor: &usize) -> usize {
-    *cursor
-  }
-  unsafe fn next_maybe(
-    cache: &mut Self, cursor: &mut usize,
-  ) -> Option<&'src TokenTree> {
-    cache.0.get(*cursor).map(|tree_span| {
-      *cursor += 1;
-      &tree_span.0
-    })
-  }
-  unsafe fn span(cache: &mut Self, range: core::ops::Range<&usize>) -> Span32 {
-    let s: Span32 = cache.0[*range.start].1;
-    let e: Span32 = cache.0[*range.end].1;
-    Span32 { start: s.start, end: e.end, context: () }
-  }
+pub struct YagParserState {
+  //todo: add read-only parser state data.
 }
 
-impl<'src> ValueInput<'src> for YagParserInput<'src> {
-  unsafe fn next(cache: &mut Self, cursor: &mut usize) -> Option<TokenTree> {
-    unsafe { Self::next_maybe(cache, cursor) }.cloned()
-  }
+pub type YagParserInput<'src> = MappedInput<
+  TokenTree,
+  Span32,
+  &'src [(TokenTree, Span32)],
+  fn(&'src (TokenTree, Span32)) -> (&'src TokenTree, &'src Span32),
+>;
+
+fn mapper<'src>(
+  (tt, span): &'src (TokenTree, Span32),
+) -> (&'src TokenTree, &'src Span32) {
+  (tt, span)
 }
 
-impl<'src> BorrowInput<'src> for YagParserInput<'src> {
-  unsafe fn next_ref(
-    cache: &mut Self, cursor: &mut usize,
-  ) -> Option<&'src TokenTree> {
-    unsafe { Self::next_maybe(cache, cursor) }
-  }
+pub fn make_yag_parser_input<'src>(
+  s: &'src [(TokenTree, Span32)],
+) -> YagParserInput<'src> {
+  let eoi: Span32 = s.last().map(|(_tt, span)| *span).unwrap_or_default();
+  Input::map(s, eoi, mapper)
 }
 
 pub trait YagParser<'src, O>:
@@ -79,8 +55,8 @@ where
 
 #[test]
 fn test_impl_return_readabilty() {
-  #[allow(unused)]
-  fn example_parser<'src, I>() -> impl YagParser<'src, ()> {
+  #[allow(dead_code)]
+  fn example_parser<'src>() -> impl YagParser<'src, ()> {
     chumsky::prelude::todo()
   }
 }
