e0838b6d4534e064d63d4040c1f9bb6ffd5bdbc9
lokathor <zefria@gmail.com>
2025-11-19 20:22:50 -0700

    separate Statement (common) and StatementKind (varying)

--- src/statement.rs
+++ src/statement.rs
@@ -1,19 +1,27 @@
 use super::*;
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
-pub enum Statement {
-  #[default]
-  StatementError,
+pub struct Statement {
+  span: Span32,
+  attribues: Vec<AstAttribute>,
+  /// use `None` when there's an error, instead of an explicit error variant.
+  kind: Option<StatementKind>,
+}
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum StatementKind {
   /// `let varname: vartype;`
-  Let(Vec<AstAttribute>, StrID, Option<StrID>),
+  Let(StrID, Option<StrID>),
 
   /// `let varname: vartype = expr;`
   ///
   /// The same as `Let` followed by an `Expr(Assign)`, but has a tag of its own
   /// so that the parser can return just one statement per `;` seperator. This
   /// should be broken up soon after the initial parsing.
-  LetAssign(Vec<AstAttribute>, StrID, Option<StrID>, Expr),
+  LetAssign(StrID, Option<StrID>, Expr),
 
-  Expr(Vec<AstAttribute>, Expr),
+  /// Any expression on its own can be a statement.
+  ///
+  /// Usually this will be an assignment, call, if-else, or loop.
+  Expr(Expr),
 }
