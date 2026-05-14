fdfb2f58a25c774cbe78334d71b313504bcb7e38
lokathor <zefria@gmail.com>
2026-05-13 20:27:58 -0600

    update tests

--- tests/all_tests/tokenizer_tests.rs
+++ tests/all_tests/tokenizer_tests.rs
@@ -1,1 +1,11 @@
 use yagbas::tokenizer::Token;
+use yagbas::tokenizer::tokenize;
+
+#[test]
+fn test_the_tokenizer() {
+  const TOKENS_ONE_PER_LINE: &str = include_str!("tokens_one_per_line.txt");
+  for line in TOKENS_ONE_PER_LINE.lines() {
+    assert_eq!(tokenize(line).count(), 1);
+    assert!(tokenize(line).all(|(t, _span)| t != Token::UnknownToken));
+  }
+}
