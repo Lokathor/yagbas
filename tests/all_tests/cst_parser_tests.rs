2bca4270a87a23f3c179f3fcaf6a145230d4f497
lokathor <zefria@gmail.com>
2026-08-27 17:32:49 -0600

    more tests

--- tests/all_tests/cst_parser_tests.rs
+++ tests/all_tests/cst_parser_tests.rs
@@ -1,9 +1,10 @@
 use yagbas::cst::Cst;
 
 #[track_caller]
-fn cst_no_errors(src: &str) {
+fn cst_no_errors(src: &str) -> Cst {
   let cst = Cst::from_module_src(src);
   cst.assert_no_errors();
+  cst
 }
 
 #[test]
@@ -22,13 +23,13 @@ fn test_trivia_module() {
 
 #[test]
 fn test_empty_function() {
-  cst_no_errors("fn foo(){}");
+  cst_no_errors("fn foo() {}");
 }
 
 #[test]
 fn test_empty_function2() {
   cst_no_errors(
-    "fn foo(){
+    "fn foo() {
     }",
   );
 }
@@ -36,8 +37,44 @@ fn test_empty_function2() {
 #[test]
 fn test_empty_function3() {
   cst_no_errors(
-    "fn foo(){
+    "fn foo() {
       // comment
+    }",
+  );
+}
+
+#[test]
+fn test_empty_let_lit_num() {
+  cst_no_errors(
+    "fn foo() {
+      let _ = 1;
+    }",
+  );
+}
+
+#[test]
+fn test_empty_let_lit_true() {
+  cst_no_errors(
+    "fn foo() {
+      let _ = true;
+    }",
+  );
+}
+
+#[test]
+fn test_empty_let_lit_false() {
+  cst_no_errors(
+    "fn foo() {
+      let _ = false;
+    }",
+  );
+}
+
+#[test]
+fn test_empty_let_lit_str() {
+  cst_no_errors(
+    "fn foo() {
+      let _ = \"abc\";
     }",
   );
 }
