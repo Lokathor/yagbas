7c5458355d7c3d4c873124ff93d11e99d06d83c5
lokathor <zefria@gmail.com>
2026-08-28 11:50:50 -0600

    more minor tests

--- tests/all_tests/cst_parser_tests.rs
+++ tests/all_tests/cst_parser_tests.rs
@@ -87,3 +87,59 @@ fn test_empty_let_unit() {
     }",
   );
 }
+
+#[test]
+fn test_assign_static() {
+  cst_no_errors(
+    "fn foo() {
+      *AUDIO_MAIN = 0;
+    }",
+  );
+}
+
+#[test]
+fn test_stmt_loop() {
+  cst_no_errors(
+    "fn foo() {
+      loop { }
+    }",
+  );
+}
+
+#[test]
+fn test_stmt_for() {
+  cst_no_errors(
+    "fn foo() {
+      for _ in 0..12 { }
+    }",
+  );
+}
+
+#[test]
+fn test_stmt_if() {
+  cst_no_errors(
+    "fn foo() {
+      if condition { x = 5; }
+    }",
+  );
+}
+
+#[test]
+fn test_const_basic() {
+  cst_no_errors("const VBLANK_START: u8 = 144;");
+}
+
+#[test]
+fn test_const_with_sub_expression() {
+  cst_no_errors("const EIGHT_ROWS_OF_TILES: u16 = TILES_PER_ROW * 8;");
+}
+
+#[test]
+fn test_static_mmio_basic() {
+  cst_no_errors("static mmio($FF44) LY: u8;");
+}
+
+#[test]
+fn test_static_mmio_array_typed() {
+  cst_no_errors("static mmio($9800) TILEMAP_9800: [u8; 32*32];");
+}
