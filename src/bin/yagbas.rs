47e7d7c157ca079f61cb07331aba1de0ee1ff49f
lokathor <zefria@gmail.com>
2026-08-28 12:23:30 -0600

    make a simple cli interface

--- src/bin/yagbas.rs
+++ src/bin/yagbas.rs
@@ -1,11 +1,72 @@
-use yagbas::cst::actions::do_module;
-use yagbas::cst::parser::CstParser;
+use std::ffi::OsString;
 
-const SOURCE: &str = include_str!("../../tests/all_tests/hello_tiles.yag");
+use yagbas::cst::Cst;
 
 fn main() {
-  let mut p = CstParser::new(SOURCE);
-  do_module(&mut p);
-  let cst = p.build_tree();
-  println!("== CST: {cst:}");
+  let arguments: Vec<_> = std::env::args_os().skip(1).collect();
+  if arguments.is_empty() {
+    eprintln!("usage: yagbas [sub_command]");
+    return;
+  }
+  match arguments[0].to_str() {
+    Some("help") | Some("--help") | Some("/?") => do_help(),
+    Some("cst") => do_cst(arguments),
+    _ => {
+      eprintln!("Unknown sub-command.");
+      do_help();
+    }
+  }
+}
+
+fn do_cst(mut arguments: Vec<OsString>) {
+  debug_assert_eq!(arguments[0].to_str().unwrap(), "cst");
+  arguments.remove(0);
+  let mut target_files = Vec::new();
+  let mut show_trivia = false;
+  for argument in arguments {
+    match argument.to_str() {
+      Some("--help") => {
+        println!("Usage: yagbas cst [args]");
+        println!("show the concrete syntax tree for one or more files");
+        println!("non-file argument options are as follows:");
+        println!(
+          "--show-trivia      print whitespace and comment tokens in the cst"
+        );
+        return;
+      }
+      Some("--show-trivia") => {
+        show_trivia = true;
+      }
+      _ => target_files.push(argument),
+    }
+  }
+  if target_files.is_empty() {
+    println!("(No filenames provided.)")
+  }
+  for target_file in target_files {
+    println!("## `{}`", target_file.display());
+    match std::fs::read_to_string(&target_file) {
+      Ok(src) => {
+        println!("```");
+        let cst = Cst::from_module_src(&src);
+        if show_trivia {
+          println!("{cst:#}");
+        } else {
+          println!("{cst}");
+        }
+        println!("```");
+      }
+      Err(e) => {
+        println!("File Reading Error: {e:?}");
+      }
+    }
+  }
+}
+
+fn do_help() {
+  println!("yagbas is an incomplete compiler.");
+  println!("Usage: yagbas [sub_command]");
+  println!("Current sub-commands are:");
+  println!(" help     this help message.");
+  println!(" cst      view the concrete syntax tree for one or more files.");
 }
