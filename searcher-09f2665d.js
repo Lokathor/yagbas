bb8bd7fb40578b28b98f6eb3d56aed196fad23f8
Lokathor <zefria@gmail.com>
2026-09-09 22:01:01 +0000

    Deploying to gh-pages from @ Lokathor/yagbas@8458d86f8288575bb590f8fcd6969e4267dfeae7 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cb72eb75.js',
+                path_to_root + 'searchindex-872f14c1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
