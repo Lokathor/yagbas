779ab372a543eb3e1e4cecb4f3ebc0debea8ee78
lokathor <zefria@gmail.com>
2026-09-13 23:19:15 -0600

    test that we really always get the exact source back

--- tests/all_tests/cst_parser_tests.rs
+++ tests/all_tests/cst_parser_tests.rs
@@ -6,6 +6,8 @@ fn cst_no_errors(src: &str) -> Cst {
   do_module(&mut p);
   let (cst, errors) = p.build_tree();
   assert!(errors.is_empty(), "Cst Parse Errors: {errors:?}");
+  let exact_src = cst.to_source_code().unwrap();
+  assert_eq!(src, exact_src);
   cst
 }
 
