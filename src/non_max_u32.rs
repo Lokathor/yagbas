7c207f9645d80b9600fce0738f7221b747a8de90
lokathor <zefria@gmail.com>
2026-09-18 13:34:31 -0600

    use +1/-1 because it in general is less likely to confuse llvm

--- src/non_max_u32.rs
+++ src/non_max_u32.rs
@@ -1,36 +1,27 @@
-use std::num::NonZeroU32;
+use core::num::NonZeroU32;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// Stores a non-maximum `u32` value, with a niche at `u32::MAX`.
+///
+/// Internally this stores a [NonZeroU32], with [try_new](NonMaxU32::try_new)
+/// applying a +1 and [get](NonMaxU32::get) applying a -1.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct NonMaxU32(NonZeroU32);
 impl NonMaxU32 {
+  /// ### Failure
+  /// * If the input is `u32::MAX`
   pub const fn try_new(x: u32) -> Option<Self> {
-    match NonZeroU32::new(x ^ u32::MAX) {
+    match NonZeroU32::new(x + 1) {
       Some(nz) => Some(Self(nz)),
       None => None,
     }
   }
   pub const fn get(self) -> u32 {
-    self.0.get() ^ u32::MAX
+    self.0.get() - 1
   }
 }
 impl Default for NonMaxU32 {
   fn default() -> Self {
     Self::try_new(0).unwrap()
-  }
-}
-impl PartialOrd for NonMaxU32 {
-  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-    Some(self.cmp(other))
-  }
-}
-impl Ord for NonMaxU32 {
-  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-    self.get().cmp(&other.get())
-  }
-}
-impl core::hash::Hash for NonMaxU32 {
-  fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-    self.get().hash(state);
   }
 }
