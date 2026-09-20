ec0c123a6f89e19dffe19d39497f3836d5af7d62
lokathor <zefria@gmail.com>
2026-09-19 22:30:23 -0600

    automated tests

--- tests/all_tests/ast_parser_tests.rs
+++ tests/all_tests/ast_parser_tests.rs
@@ -1,19 +1,34 @@
-use str_id::StrId;
-use yagbas::ast::AstModule;
+use std::path::PathBuf;
+
+use yagbas::{
+  ast::{Ast, actions::parse_ast_module, parser::AstParser},
+  cst::{
+    actions::gather_module,
+    parser::{BuildTreeArgs, CstParser},
+  },
+};
 
 #[track_caller]
-fn module_no_errors(src: &str) -> AstModule {
-  let origin = StrId::from("<InMemoryTest>");
-  let ast = AstModule::from_source(origin, src);
-  assert!(!ast.has_errors(), "===Ast Has Errors: {ast:?}");
+fn ast_no_errors(src: &str) -> Ast {
+  let file_origin = PathBuf::from("InMemoryData");
+  let mut p = CstParser::new(&src);
+  gather_module(&mut p);
+  let cst = p.build_tree(BuildTreeArgs { skip_trivial: true });
+  let mut ast_parser =
+    AstParser { file_origin: file_origin.clone(), errors: Vec::new() };
+  let module = parse_ast_module(&mut ast_parser, file_origin, &cst);
+  let mut ast = Ast::default();
+  ast.modules.push(module);
+  ast.errors.extend(ast_parser.errors);
+  assert!(ast.errors.is_empty(), "{:?}", ast.errors);
   ast
 }
 
 #[test]
 fn test_empty_module() {
-  module_no_errors("");
-  module_no_errors(" ");
-  module_no_errors(
+  ast_no_errors("");
+  ast_no_errors(" ");
+  ast_no_errors(
     "
     // comment
   ",
@@ -22,23 +37,23 @@ fn test_empty_module() {
 
 #[test]
 fn test_const() {
-  module_no_errors("const VBLANK_START: u8 = 144;");
-  module_no_errors("const EIGHT_ROWS_OF_TILES: u16 = TILES_PER_ROW * 8;");
+  ast_no_errors("const VBLANK_START: u8 = 144;");
+  ast_no_errors("const EIGHT_ROWS_OF_TILES: u16 = TILES_PER_ROW * 8;");
 }
 
 #[test]
 fn test_static_mmio() {
-  module_no_errors("static mmio($FF44) LY: u8;");
-  module_no_errors("static mmio($9800) TILEMAP_9800: [u8; 32*32];");
-  module_no_errors(
+  ast_no_errors("static mmio($FF44) LY: u8;");
+  ast_no_errors("static mmio($9800) TILEMAP_9800: [u8; 32*32];");
+  ast_no_errors(
     "static mmio($8800) VRAM_BLOCK_8000: [[u8;BYTES_PER_TILE]; TILES_PER_BLOCK];",
   );
 }
 
 #[test]
 fn test_function_empty() {
-  module_no_errors("fn foo() {}");
-  module_no_errors(
+  ast_no_errors("fn foo() {}");
+  ast_no_errors(
     "fn foo() {
       // comment
     }",
@@ -47,7 +62,7 @@ fn test_function_empty() {
 
 #[test]
 fn test_function_assign() {
-  module_no_errors(
+  ast_no_errors(
     "fn foo() {
       *AUDIO_MAIN = 0;
     }",
@@ -56,12 +71,12 @@ fn test_function_assign() {
 
 #[test]
 fn test_function_let() {
-  module_no_errors(
+  ast_no_errors(
     "fn foo() {
       let tile0 = &VRAM_BLOCK_8000[0][0];
     }",
   );
-  module_no_errors(
+  ast_no_errors(
     "fn foo() {
       let pattern = $AA;
     }",
@@ -70,7 +85,7 @@ fn test_function_let() {
 
 #[test]
 fn test_function_loop() {
-  let m = module_no_errors(
+  ast_no_errors(
     "fn foo() {
       loop {
         if *LY == VBLANK_START {
@@ -80,20 +95,11 @@ fn test_function_loop() {
       *LCDC = 0;
     }",
   );
-  let item = &m.items[0];
-  match &item.kind {
-    yagbas::ast::AstItemKind::Function(f) => {
-      // check that the loop gets properly made a separate statement from the
-      // line after it, because the brace gets an implicit semicolon separator.
-      assert_eq!(f.body.statements.len(), 2)
-    }
-    _ => panic!(),
-  }
 }
 
 #[test]
 fn test_function_for() {
-  let m = module_no_errors(
+  ast_no_errors(
     "fn foo() {
       for _ in 0..BYTES_PER_TILE {
         *tile1 = pattern;
@@ -102,20 +108,11 @@ fn test_function_for() {
       *LCDC = 0;
     }",
   );
-  let item = &m.items[0];
-  match &item.kind {
-    yagbas::ast::AstItemKind::Function(f) => {
-      // check that the loop gets properly made a separate statement from the
-      // line after it, because the brace gets an implicit semicolon separator.
-      assert_eq!(f.body.statements.len(), 2)
-    }
-    _ => panic!(),
-  }
 }
 
 #[test]
 fn test_function_if() {
-  let m = module_no_errors(
+  ast_no_errors(
     "fn foo() {
       if *LY == VBLANK_START {
         break
@@ -123,13 +120,4 @@ fn test_function_if() {
       *LCDC = 0;
     }",
   );
-  let item = &m.items[0];
-  match &item.kind {
-    yagbas::ast::AstItemKind::Function(f) => {
-      // check that the loop gets properly made a separate statement from the
-      // line after it, because the brace gets an implicit semicolon separator.
-      assert_eq!(f.body.statements.len(), 2)
-    }
-    _ => panic!(),
-  }
 }
